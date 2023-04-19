6e7c03c188a692754415de04be7c83bd625365c9
KseniaBurkhanova <100416320+KseniaBurkhanova@users.noreply.github.com>
2023-04-19 13:31:44 +0300

    change .net version from 7.0 to 6.0

---
+++ math/Program.cs
@@ -0,0 +1,6 @@
+﻿
+double d = Math.Sqrt(5);
+
+double d1 = 5.09987654;
+double d1Round = Math.Round(d1, 2, MidpointRounding.ToZero);
+Console.WriteLine(d1Round);
---
+++ task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет 
+// 161 -> да
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultipleTwoDigits(numberA);
+Console.WriteLine(result ? "да" : "нет");
+bool MultipleTwoDigits(int num)
+{
+     return numberA %7 == 0 && num % 23 == 0;
+}
